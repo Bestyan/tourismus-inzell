94b2885a1fb94f905fc6cc87403366035c565d47
Bastian Schattenberg <git@bestyan.de>
2020-07-28 19:29:27 +0200

    chore: reduce navigation to essentials

---
+++ layouts/navigation/NavDrawer.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-navigation-drawer
+    v-model="drawer"
+    :clipped="clipped"
+    fixed
+    app
+  >
+    <v-list>
+      <v-list-item
+        v-for="(item, i) in items"
+        :key="i"
+        :to="item.to"
+        router
+        exact
+      >
+        <v-list-item-action>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title v-text="item.title" />
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  name: 'NavDrawer',
+  props: {
+    items: {
+      type: Array,
+      required: true,
+    },
+
+    clipped: {
+      type: Boolean,
+      required: true,
+    },
+
+    drawer:{
+        type: Boolean
+    }
+  },
+}
+</script>
+
+<style></style>
