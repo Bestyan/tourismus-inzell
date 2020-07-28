dc3dcd851b0db501315d73d0823b5d339316546e
Bastian Schattenberg <git@bestyan.de>
2020-07-28 20:23:07 +0200

    feat: /attraktionen with basic masonry

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,6 +37,7 @@ export default {
   ** https://nuxtjs.org/guide/plugins
   */
   plugins: [
+    '@/plugins/vue-masonry-css.js'
   ],
   /*
   ** Auto import components
---
+++ pages/attraktionen.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <h1>Attraktionen</h1>
+    <masonry
+      :cols="{ default: 4, 1000: 3, 700: 2, 400: 1 }"
+      :gutter="{ default: '30px', 700: '15px' }"
+    >
+      <div v-for="(item, index) in items" :key="index" class="item">
+        <img :src="item.image" />
+
+        <div class="content">
+          <h3>{{ item.title }}</h3>
+          <p>{{ item.content }}</p>
+        </div>
+      </div>
+    </masonry>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+
+  data() {
+    return {
+      items: [
+        {
+          title: 'Max Aicher Arena',
+          content:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas non sagittis leo. Vestibulum sit amet metus nec neque dignissim dapibus.',
+          image:
+            'https://www.max-aicher-arena.de/wp-content/uploads/BAN_Herbst.jpg',
+        },
+        {
+          title: 'Salzbergwerk Berchtesgaden',
+          content:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id mollis erat. Aliquam erat volutpat. Nunc erat lacus, rhoncus nec.',
+          image:
+            'https://www.salzbergwerk.de/salzbergwerk/blog/content/Wiederer%C3%B6ffnung/image-thumb__2096__ebb_hero-teaser-img/Magischer%20Salzraum.pjpeg',
+        },
+        {
+          title: 'Weißbachschlucht',
+          content:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ac diam ac ex efficitur posuere. Pellentesque cursus pellentesque risus, non.',
+          image:
+            'https://www.berchtesgaden.de/cdn/uploads/weissbachschlucht-in-schneizlreuth-thmediafilelightbox.jpg',
+        },
+      ],
+    }
+  },
+
+  methods: {
+    /**
+     * I am mocking a API call that load 20 objects at a time.
+     */
+    append() {},
+  },
+}
+</script>
+
+<style>
+.item {
+  overflow: hidden;
+  border-radius: 4px;
+  width: 100%;
+  background: rgb(53, 53, 53);
+}
+.content {
+  padding: 20px;
+}
+img {
+  object-fit: cover;
+  width: 100%;
+  height: 100%;
+  line-height: 0;
+  display: block;
+}
+</style>
