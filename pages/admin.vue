95d59513f7281bb0d27eac41526f4124a0e905ae
Bastian Schattenberg <git@bestyan.de>
2020-08-01 03:10:34 +0200

    feat: /admin can upload file

--- pages/admin.vue
+++ pages/admin.vue
@@ -1,13 +1,35 @@
 <template>
-  
+  <div>
+    <v-file-input
+      filled
+      :show-size="1000"
+      placeholder="Excel Datei mit Attraktionen"
+      v-model="attractionFile"
+    />
+    <v-btn @click="upload">Hochladen</v-btn>
+  </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      attractionFile: null,
+    }
+  },
 
+  methods: {
+    upload(event) {
+      console.log(this.attractionFile)
+      let formData = new FormData();
+      formData.append('file', this.attractionFile)
+      this.$axios
+        .put('/admin/upload', formData)
+        .then(result => console.log(result))
+        .catch(error => console.log(error))
+    },
+  },
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
